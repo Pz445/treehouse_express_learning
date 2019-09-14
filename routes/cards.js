b350cddd3d612d4c30a70f15d35ee0bcac03f185
Pat P <sell445@gmail.com>
2019-09-13 20:51:20 -0400

    added JS logic to cards route

--- routes/cards.js
+++ routes/cards.js
@@ -3,18 +3,26 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+  let num = Math.floor(Math.random() * cards.length);
+  res.redirect(`/cards/${num}?side=question`);
+})
 
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
   const text = cards[id][side];
   const { hint } = cards[id];
-  const templateData = { text };
+  const templateData = { id, text };
 
   if (side === 'question') {
     templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = "Answer"
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
   }
-  console.log(templateData)
   res.render('card', templateData);
 });
 
